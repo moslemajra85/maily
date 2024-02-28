e8b4b4d15ef9db8b56b5305cea00869db6741bc4
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2024-02-28 16:03:19 +0100

    change the code to test deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ continue: "learning" });
 });
 
 const PORT = process.env.PORT || 5000;
